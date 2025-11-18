6d434b34b280aa400050ffdadb2ec01496f37e2a
aaliyahazzahra <aariariaa02@gmail.com>
2025-11-18 11:20:57 +0700

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_absensi_ppkd_b4/presentation/view/auth/login_page.dart';
 import 'package:project_absensi_ppkd_b4/provider/attendance_provider.dart';
+import 'package:project_absensi_ppkd_b4/provider/auth_provider.dart';
 import 'package:project_absensi_ppkd_b4/provider/dropdown_provider.dart';
 import 'package:project_absensi_ppkd_b4/provider/profile_provider.dart';
 import 'package:project_absensi_ppkd_b4/repositories/attendance_repository.dart';
@@ -11,33 +12,21 @@ import 'package:project_absensi_ppkd_b4/service/api_service.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         Provider<ApiService>(create: (_) => ApiService()),
 
         ProxyProvider<ApiService, AuthRepository>(
           update: (_, apiService, __) => AuthRepository(apiService: apiService),
         ),
-
-        ProxyProvider<ApiService, ProfileRepository>(
-          update: (_, apiService, __) =>
-              ProfileRepository(apiService: apiService),
-        ),
-
-        ProxyProvider<ApiService, DropdownRepository>(
-          update: (_, apiService, __) =>
-              DropdownRepository(apiService: apiService),
-        ),
-
-        ProxyProvider<ApiService, AttendanceRepository>(
-          update: (_, apiService, __) =>
-              AttendanceRepository(apiService: apiService),
-        ),
-
-        ProxyProvider<ApiService, AuthRepository>(
-          update: (_, apiService, __) => AuthRepository(apiService: apiService),
-        ),
         ProxyProvider<ApiService, ProfileRepository>(
           update: (_, apiService, __) =>
               ProfileRepository(apiService: apiService),
@@ -49,6 +38,14 @@ void main() {
         ProxyProvider<ApiService, AttendanceRepository>(
           update: (_, apiService, __) =>
               AttendanceRepository(apiService: apiService),
+        ),
+
+        ChangeNotifierProxyProvider<AuthRepository, AuthProvider>(
+          create: (context) => AuthProvider(),
+          update: (context, repository, previousProvider) {
+            previousProvider!.updateRepository(repository);
+            return previousProvider;
+          },
         ),
         ChangeNotifierProxyProvider<ProfileRepository, ProfileProvider>(
           create: (context) => ProfileProvider(),
@@ -57,7 +54,6 @@ void main() {
             return previousProvider;
           },
         ),
-
         ChangeNotifierProxyProvider<DropdownRepository, DropdownProvider>(
           create: (context) => DropdownProvider(),
           update: (context, repository, previous) {
@@ -65,7 +61,6 @@ void main() {
             return previous;
           },
         ),
-
         ChangeNotifierProxyProvider<AttendanceRepository, AttendanceProvider>(
           create: (context) => AttendanceProvider(),
           update: (context, repository, previous) {
@@ -74,129 +69,15 @@ void main() {
           },
         ),
       ],
-      child: const MyApp(),
-    ),
-  );
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Builder(
-        builder: (context) {
-          // 'context' ini dijamin berada DI DALAM MaterialApp,
-          // yang berarti juga DI BAWAH MultiProvider.
-          return const LoginPage();
-        },
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
+
+        home: const LoginPage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
